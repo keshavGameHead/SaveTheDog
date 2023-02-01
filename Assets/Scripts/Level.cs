db32d391c69f8a2719f973b5baf7aa4db52b69f6
Riken Sonani <66293545+keshavGameHead@users.noreply.github.com>
2023-02-01 15:33:35 +0530

    change Background

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Level : MonoBehaviour
 {
     public int levelIndex;
+    public bool isDarkBg = false;
 
     public List<Transform> dogList;
 
@@ -12,7 +14,14 @@ public class Level : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (isDarkBg)
+        {
+            UIManager.Instance.Bg.GetComponent<SpriteRenderer>().color = new Color32(85, 85, 85, 255);
+        }
+        else
+        {
+            UIManager.Instance.Bg.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 255);
+        }
     }
 
     // Update is called once per frame
@@ -20,4 +29,6 @@ public class Level : MonoBehaviour
     {
         
     }
+
+
 }
